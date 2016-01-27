24bce2d9d66cf6e639a5d9ee674c9de784afd38a
Andy Zhang <andyzhang1992@gmail.com>
2016-01-27 12:58:49 -0800

    Began implementing dll reference on runtime for sources

--- Source/SourceManager.cs
+++ Source/SourceManager.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Source
 {
     public enum SourceLocation { Web69, WebPiaoTian, WuxiaWorld };
+
+    public class PlugInFactory<T>
+    {
+        public T CreatePlugin(string path)
+        {
+            foreach(string file in Directory.GetFiles(path, "*.dll"))
+            {
+                foreach (Type assemblyType in Assembly.LoadFrom(file).GetTypes())
+                {
+                    Type interfaceType = assemblyType.GetInterface(typeof(T).FullName);
+                    if (interfaceType != null)
+                        return (T)Activator.CreateInstance(assemblyType);
+                }
+            }
+            return default(T);
+        }
+    }
 
     public class SourceManager
     {
